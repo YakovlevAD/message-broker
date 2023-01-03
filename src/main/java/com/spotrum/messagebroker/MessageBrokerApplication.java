23161a973c63262ba2589d2fba525e6ff0ccee6e
Shanji <ds@gmail.com>
2023-01-03 23:15:55 +0500

    DD-24539845: init db

--- src/main/java/com/spotrum/messagebroker/MessageBrokerApplication.java
+++ src/main/java/com/spotrum/messagebroker/MessageBrokerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MessageBrokerApplication {
 
+	//for build
 	public static void main(String[] args) {
 		SpringApplication.run(MessageBrokerApplication.class, args);
 	}
